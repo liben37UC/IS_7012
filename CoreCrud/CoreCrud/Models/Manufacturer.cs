25d1b91a3e842918faa56d18896a0cb8e82753bd
liben37UC <pliben1994@gmail.com>
2019-09-18 16:49:07 -0400

    new corecrud assignment 3

---
+++ CoreCrud/CoreCrud/Models/Manufacturer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCrud.Models
+{
+    public class Manufacturer
+    {
+        public int ID { get; set; }
+
+        public string ManufacturerName { get; set; }
+
+        public ICollection<Collectible> Collectibles { get; set; }
+
+        [NotMapped]
+        public string DisplayName
+        {
+            // CONCATENATE THE FIRST AND LAST NAME
+            // INTO A DISPLAY NAME
+            get { return $"{ID}"; }
+        }
+    }
+}
